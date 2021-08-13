bb83486b817def69b259a810274c97bd21d7c0eb
kseniajasko@gmail.com <kseniajasko555>
2021-08-13 12:24:43 +0300

    added hw11(task1)

--- hw11/hw11_task_1.py
+++ hw11/hw11_task_1.py
@@ -1,6 +1,9 @@
 # Task 1
 #
-# Make a class structure in python representing people at school. Make a base class called Person, a class called Student, and another one called Teacher. Try to find as many methods and attributes as you can which belong to different classes, and keep in mind which are common and which are not. For example, the name should be a Person attribute, while salary should only be available to the teacher.
+# Make a class structure in python representing people at school. Make a base class called Person, a class called Student,
+# and another one called Teacher. Try to find as many methods and attributes as you can which belong to different classes,
+# and keep in mind which are common and which are not. For example, the name should be a Person attribute,
+# while salary should only be available to the teacher.
 
 class Person():
     def __init__(self, first_name, last_name, age, phone_number, email):
