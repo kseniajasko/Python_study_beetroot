71e6205706006fbe577339b2d08cf192a10e06ea
kseniajasko@gmail.com <kseniajasko555>
2021-09-08 13:23:34 +0300

    added hw20

--- hw20/hw20_task_1.py
+++ hw20/hw20_task_1.py
@@ -2,24 +2,49 @@
 # Also, you need to extend its functionality with counter and logging.
 # Pay special attention to the implementation of `__exit__` method.
 import logging
+from os import path
+
+logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
 class File(object):
+    counter = 0
+
+    @classmethod
+    def get_counter(cls):
+        return cls.counter
 
     def __init__(self, file_name, method):
+        File.counter += 1
         self.file_obj = open(file_name, method)
+        logging.warning(f'Getting the parametres: {File.counter}')
 
     def __enter__(self):
+        logging.warning(f'Opens the file and returns it: {File.counter}')
         return self.file_obj
 
     def __exit__(self, type, value, traceback):
+        logging.warning(f'Closes the file: {File.counter}')
         self.file_obj.close()
 
-def write(text_1, file_1):
-    with File(file_1, 'r+') as opened_file:
-        opened_file.write(text_1)
+def write(text_1: str, file_1: str):
+    with File(file_1, 'a') as f_1:
+        f_1.write(f'{text_1}\n')
 
-write('Hola', 'demo.txt')
-write('Hola1', 'demo.txt')
+def reads(file_2: str):
+    if not path.isfile(file_2):
+        raise FileNotFoundError(f'The file "{file_2}" does not exist')
+
+    with File(file_2, 'r+') as f_2:
+        a = f_2.read()
+        return a
+
+if __name__ == '__main__':
+    write('One', 'demo.txt')
+    #write('Two', 'demo.txt')
+    reads('demo.txt')
+    reads('demo1.txt')
+
+    print(File.get_counter())
 
 
 # 1) The with statement stores the __exit__ method of the File class.
--- hw20/hw20_test_task_1.py
+++ hw20/hw20_test_task_1.py
@@ -1,15 +1,16 @@
 import unittest
 import hw20_task_1
+import logging
 
 class TestTaskOne(unittest.TestCase):
 
     def test_add(self):
         file_test = 'demo.txt'
-        test_text = 'Hola'
+        test_text = f'Two\n'
         hw20_task_1.write(test_text, file_test)
-        f_1 = open(file_test, 'r+')
-        a_1 = f_1.readline()
-        print(a_1)
+        with open(file_test, 'r+') as f_1:
+            a_1 = f_1.readline()
+        #a_1 = hw20_task_1.reads(file_test)
         self.assertEqual(test_text, a_1)
 
 unittest.main()
